b4a43f88e07ad7678224c58e5d07bd1685ab5044
Mahmoud Fazeli <mahmoud.fazeli@dpschool.io>
2023-01-21 20:59:16 +0100

    Modify the context

--- flask_app.py
+++ flask_app.py
@@ -6,13 +6,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+# Create a global variable to store the previous response
+prev_response = ""
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/send_prompt', methods=['POST'])
 def send_prompt():
+    global prev_response
     prompt = json.loads(request.data)['prompt']
+    # Append the previous response to the prompt
+    prompt = prev_response + prompt
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -21,7 +27,9 @@ def send_prompt():
         stop=None,
         temperature=0.7,
     )
-    return response["choices"][0]["text"]
+    # Update the previous response to be the current response
+    prev_response = response["choices"][0]["text"]
+    return prev_response
 
 if __name__ == '__main__':
     app.run()
