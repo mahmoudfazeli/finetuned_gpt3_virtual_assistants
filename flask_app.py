b7a987e5e8b6f326b33aa6b9da8506e94e0d1038
Mahmoud Fazeli <mahmoud.fazeli@dpschool.io>
2023-01-22 03:23:39 +0100

    Add select model

--- flask_app.py
+++ flask_app.py
@@ -20,16 +20,37 @@ def send_prompt():
     # Append the previous response to the prompt
     prompt = prev_response + prompt
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        model=model,
         prompt=prompt,
-        max_tokens=2048,
-        n=1,
-        stop=None,
         temperature=0.7,
-    )
+        max_tokens=60,
+        top_p=0.3,
+        frequency_penalty=0.5,
+        presence_penalty=0,
+        n=1,
+        stop=None
+    )   
     # Update the previous response to be the current response
     prev_response = response["choices"][0]["text"]
     return prev_response
 
 if __name__ == '__main__':
+    # Define a list of valid model names
+    valid_models = ["text-davinci-003", "text-curie-001", "text-babbage-001", "text-ada-001"]
+
+    # Prompt the user for a model name
+    while True:
+        print("Please select a model:")
+        for i, model_name in enumerate(valid_models):
+            print(f"{i+1}. {model_name}")
+        print("4. Enter custom model name")
+        choice = input("Enter the number of your choice: ")
+        if choice == "4":
+            model = input("Enter a custom model name: ")
+            break
+        elif int(choice) in range(1,4):
+            model = valid_models[int(choice)-1]
+            break
+        else:
+            print("Invalid choice. Please try again.")
     app.run()
